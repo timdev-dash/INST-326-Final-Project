cea4a8d113eff20e2960a42592f3c90c56a588d7
KelemiYee <115328674+KelemiYee@users.noreply.github.com>
2022-12-16 23:32:22 -0500

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -6,23 +6,38 @@ INST326-ESG1 Farmer Fall 2022
 """
 '''
 Purpose: 
-    - To look up the cost of specific medicaid drugs using data from a source. Initially we hoped to use web scraping for this, however given the complexity we needed to find an alternate source.
-
+    - To look up the cost of specific medicaid drugs using data from a source. Initially we hoped to use web scraping for this, however given the complexity
+        we needed to find an alternate source.
 Challenges
     - Originally our plan was to compare product's prices from two different website, but due to the time constraint we did an alternative program.
     - Difficulty reading csv file from the medicaid website
     - csv file does not contain price values. The NCPDP Billing unit Standard fact sheet explains the pricing unit for each medicine 
-
 '''
 import argparse
 import sys
 import pandas as pd
 
+class Medication:
+    '''This class will create a Medication object using the primary parse as the self'''
+    
+    def __init__(self, medication):
+        self.medication = medication
+    
+    def MedicineOutput(self, MedicineFrame):
+        '''
+        Functionality:
+        - A function stores the NDC description 
+        
+        Return:
+        - Specific_Medicine: Dataframe of specific medicine '''
+        ## stores ndc description as a string 
+        Specific_Medicine = MedicineFrame[MedicineFrame["NDC Description"].str.contains(self.medication)]
+        return Specific_Medicine
+
 def Dataframe():
     '''
     Functionality:
         - A function that takes data from a csv url and converts it into a dataframe
-
     Return:
         - a dataframe called Medicines  
     '''
@@ -34,13 +49,11 @@ def Cleanframe(dataframe):
     '''
     Functionality:
         - A function that cleans the dataframe of unnecessary columns by removing columns from the original dataframe
-
     Arguments: 
         - dataframe: Dataframe containing items and their prices of NDC
     
     Returns:
         - cleaned_dataframe: A cleaned dataframe
-
     '''
     ## selected columns that are deemed unnecessary ##
     cols = [3,5,6,7,8,9,10,11]
@@ -52,32 +65,14 @@ def Uniquefilter(duplicates):
     '''
     Functionality:
         - A function that filters duplicate rows so only unquie rows remain in the dataframe
-
     Argument:
         - dupliates: identical rows in the dataframe
-
     Returns:
         - Unique_Frame: A dataframe with only unique rows
     '''
     ## drop duplicate rows in the dataframe ##
     Unique_Frame = duplicates.drop_duplicates(subset = "NDC Description")
     return Unique_Frame
-
-def MedicineOutput(MedicineFrame, Medicine):
-    '''
-    Functionality:
-        - A function that stores the NDC description 
-
-    Arguments:
-        - MedicineFrame: Sorted and filter dataframe
-        - Medicine: Dataframe of national drug data
-    
-    Return:
-        - Specific_Medicine: Dataframe of specific medicine
-    '''
-    ## stores ndc description as a string 
-    Specific_Medicine = MedicineFrame[MedicineFrame["NDC Description"].str.contains(Medicine)]
-    return Specific_Medicine
 
 def FileWriter(dataframe):
     '''
@@ -98,10 +93,8 @@ def string_writer(dataframe):
     '''
     Functionality:
         - A function that covert dataframe to a list 
-
     Argument:
         - dataframe: dataframe containing items and their prices of NDC
-
     Return:
         - Medicine_List: A list of medicines 
     '''
@@ -113,17 +106,19 @@ def string_writer(dataframe):
 def main(primary):
     '''
     Functionality:
-        - Create an instance for each created function and save them to corresponding variables
+        - Create an instance of the server-class using the created functions and save them to corresponding variables
     
     Argument:
         - primary: Command-line arguments for the program
-
     Return:
         - Medicine_List: A list containing selected medicine, price and price_unit
     '''
     ## create instance of Dataframe function ##
     MedicineFrame = Dataframe()
-
+    
+    ## create Medication class object ##
+    Medication_Type = Medication(primary)
+    
     ## create instance of Cleanframe function using the given MedicineFrame ##
     CleanMedicineFrame = Cleanframe(MedicineFrame)
 
@@ -131,7 +126,7 @@ def main(primary):
     UniqueMedicineFrame = Uniquefilter(CleanMedicineFrame)
     
     ## create instance of MedicineOutput function using the given UniqueMedicineFrame and primary ##
-    SpecificMedicine = MedicineOutput(UniqueMedicineFrame, primary)
+    SpecificMedicine = Medication_Type.MedicineOutput(UniqueMedicineFrame)
 
     ## create instance of string_writer function using the given SpecificMedicine ##
     Medicine_List = string_writer(SpecificMedicine)
@@ -145,10 +140,8 @@ def parse_args(args_list):
     Functionality:
         - Create an instance of the ArgumentParser class from the argparse module
         - Use the add_argument() method of your ArgumentParser instance to add primary arguement
-
     Argument:
         - arg_list : a list of strings containing the command-line arguments for the program
-
     Returns:
         - The ArgumentParser object created
     """
